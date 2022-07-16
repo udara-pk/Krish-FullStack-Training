4d3bfa23895db373958f7e2281d406c3b97df245
IT18128482 <it18128482@my.sliit.lk>
2022-07-17 00:59:05 +0530

    Config-Consumer

---
+++ Microservice/config-consumer/src/test/java/com/udara/configconumer/configconsumer/ConfigConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udara.configconumer.configconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
