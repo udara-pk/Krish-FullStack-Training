da6cbd074e242510f636fed9160bf9eb7835c91c
IT18128482 <it18128482@my.sliit.lk>
2022-07-24 19:31:33 +0530

    Fully Completed WaterDropQuestion2

--- Water Dropping Problem 2/WaterDrop2/src/WaterDropQues2.java
+++ Water Dropping Problem 2/WaterDrop2/src/WaterDropQues2.java
@@ -16,78 +16,65 @@ public class WaterDropQues2 {
 	            { 0, 0, 0, 0, 0, 0, 1 },
 	            { 0, 0, 0, 0, 0, 0, 0 },
 			};
-
-
-
+		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Please Enter Position for Water dropping:");
 		int position=sc.nextInt() - 1;//possition=3
 		bucketArray[position] = inputValue;//{ 0, 0, 0, 100, 0, 0, 0 }
-		double [] currCalculated;
+		double [] currCalculated=new double []{0,0,0,0,0,0,0};
+		double [] tempCalculated=new double [] {0,0,0,0,0,0,0};
 		
-		for (int i = 0; i < waterPattern.length; i++) {
-            int [] currentRow = waterPattern[i];
-            int [] nextRow = waterPattern[i+1];
-            double [] waterDivideValues;
-
-            currCalculated = Arrays.copyOf(bucketArray, bucketArray.length);//{ 0, 0, 0, 100, 0, 0, 0 }
-
-            for (int j = position; j < currentRow.length; j++) {
-                if (nextRow[j] == 1 && bucketArray[j] > 0){
-                    waterDivideValues = waterSupplier(currentRow.length, bucketArray[j], currentRow[j], j);
-
-                    // check right
-                    if (waterDivideValues[1] > 0){
-                        currCalculated[j + 1] = currCalculated[j + 1] + waterDivideValues[1];
-                    }
-                    currCalculated[j] = 0;
-                }
-                
-            }
-            
-            for (int j = position; j <= 0; j--) {
-                if (nextRow[j] == 1 && bucketArray[j] > 0){
-                    waterDivideValues = waterSupplier(currentRow.length, bucketArray[j], currentRow[j], j);
-
-                    // check left
-                    if (waterDivideValues[0] > 0){
-                        currCalculated[j - 1] = currCalculated[j - 1] + waterDivideValues[0];
-                    }
-
-                    
-                    currCalculated[j] = 0;
-                }
-                
-            }
-            
-            // update basket array
-            bucketArray = currCalculated;
-            
-        }
-
         // print
-        System.out.println("Result:");
+        /*System.out.println("Result:");
+        for (double bucket: bucketArray ) {
+            System.out.print(bucket+ ", ");
+        }*/	
+		double percentage;
+		
+		for(int i=0; i< waterPattern.length;i++) {
+			for(int j=0;j<bucketArray.length;j++) {
+				if(bucketArray[j]>0) {
+					percentage=bucketArray[j];
+					if(waterPattern[i][j]==1) {
+						//Turn Right
+						for(int k=j;k<waterPattern.length;k++) {
+							if(waterPattern[i][k]==0 && waterPattern[i-1][j+1]!=1) {
+								currCalculated[k]=currCalculated[k]+percentage/2;
+								currCalculated[j]=0;
+								break;
+							}
+						}
+						//Turn Left
+						for(int k=j;k>=0;k--) {
+							if(waterPattern[i][k]==0 && waterPattern[i-1][j-1]!=1) {
+								currCalculated[k]=currCalculated[k]+percentage/2;
+								currCalculated[j]=0;
+								break;
+							}
+						}
+						currCalculated[j]=0;
+					}
+					else {
+						currCalculated[j]=percentage;
+					}
+				}
+			}
+			if(!Arrays.equals(tempCalculated,currCalculated)) {
+				bucketArray=currCalculated;
+				tempCalculated=currCalculated;
+			}
+			//bucketArray=currCalculated;
+		}
+		
+		System.out.println("Result:");
         for (double bucket: bucketArray ) {
             System.out.print(bucket+ ", ");
         }
     }
+	
+	
 
-    public static double[] waterSupplier(int colCount,double inputValue, int bucketValue, int bucketColIndex){
-        double [] waterDivideValues = {0,0};
-        if (bucketValue == 1){
-            // split now
-            if(bucketColIndex == 0){
-                waterDivideValues[1]  = inputValue / 2;
-            }
-            else if (bucketColIndex == colCount - 1){
-                waterDivideValues[0]  = inputValue / 2;
-            }else{
-                waterDivideValues[0] = inputValue / 2;
-                waterDivideValues[1] = inputValue / 2;
-            }
-        }
-        return waterDivideValues;
-    }
+    
 	
 
 }
