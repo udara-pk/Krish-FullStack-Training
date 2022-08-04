f2c62b6be098399eb7360ed92f0170df39825cd2
IT18128482 <it18128482@my.sliit.lk>
2022-08-04 12:49:49 +0530

    Enable Eureka Client

--- Fuel Distribution Application/FuelDistributionApp/src/main/java/com/udara/fuelapp/fueldistributionapp/FuelDistributionAppApplication.java
+++ Fuel Distribution Application/FuelDistributionApp/src/main/java/com/udara/fuelapp/fueldistributionapp/FuelDistributionAppApplication.java
@@ -2,8 +2,10 @@ package com.udara.fuelapp.fueldistributionapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FuelDistributionAppApplication {
 
     public static void main(String[] args) {
