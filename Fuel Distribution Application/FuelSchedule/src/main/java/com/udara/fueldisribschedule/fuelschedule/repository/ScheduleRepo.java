29215b21200f43aeeb1b0933ad221da603146301
IT18128482 <it18128482@my.sliit.lk>
2022-08-08 16:23:05 +0530

    Update Schedule Service

--- Fuel Distribution Application/FuelSchedule/src/main/java/com/udara/fueldisribschedule/fuelschedule/repository/ScheduleRepo.java
+++ Fuel Distribution Application/FuelSchedule/src/main/java/com/udara/fueldisribschedule/fuelschedule/repository/ScheduleRepo.java
@@ -2,7 +2,9 @@ package com.udara.fueldisribschedule.fuelschedule.repository;
 
 import com.udara.fueldisribschedule.fuelschedule.model.Schedule;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScheduleRepo extends MongoRepository<Schedule, Integer> {
 
 }
